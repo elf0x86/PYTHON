6b39bf8fb08a5ca7b39273580b8b5c30010656b4
sampiano <samcodebeliver@gmail.com>
2024-05-08 18:34:47 -0300

    Update aula06b.py

--- aula06b.py
+++ aula06b.py
@@ -1,2 +1,5 @@
 n = input('Digite algo: ')
 print('valor digitado é numerico? ', n.isnumeric())
+
+n1 = float(input("Digite outro valor numerico: "))
+print("valor digitado é numerico? ", n1.isnumeric())
