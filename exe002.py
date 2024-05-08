e0766075adbdb060009bb96ea3c0832f15a18bd0
sampiano <samcodebeliver@gmail.com>
2024-05-08 16:35:31 -0300

    Create exe002.py

---
+++ exe002.py
@@ -0,0 +1,8 @@
+# Crie um script que leia o dia, o mes, e o ano de nascimento de uma pessoa
+# mostre uma mensagem com a data formatada
+
+dia = input("Dia = ")
+mes = input("Mes = ")
+ano = input("Ano = ")
+
+print(f"Você nasceu no dia {dia} de {mes} de {ano}.")
