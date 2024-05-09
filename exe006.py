3568a7f6441ab5faa7b44e652709140726402799
samsorce <samcodebeliver@gmail.com>
2024-05-09 10:07:05 -0300

    Create exe006.py

---
+++ exe006.py
@@ -0,0 +1,8 @@
+# Leia um número e mostre o seu dobro, triplo, e raiz quadrada
+
+
+numero = int(input("Entre com um número: "))
+
+print(f"O dobro  de {numero} = {numero * 2}")
+print(f"O triplo de {numero} = {numero * 3}")
+print(f"A raiz²  de {numero} = {numero**(1/2)}")
