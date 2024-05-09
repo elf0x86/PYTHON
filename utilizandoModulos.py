1df296cda770faa33c6e0c4a577a67489716538b
samsorce <samcodebeliver@gmail.com>
2024-05-09 17:06:53 -0300

    Create  utilizandoModulos.py

---
+++ utilizandoModulos.py
@@ -0,0 +1,21 @@
+import math
+num = int(input("Digite um número: "))
+raiz = math.sqrt(num)
+print(f"A raiz de {num} é igual a {raiz}")
+
+
+from math import sqrt, floor
+num = int(input("Digite um número: "))
+raiz = sqrt(num)
+print(f"A raiz de {num} é igual a {floor(raiz)}")
+
+# como saber quais bibliotecas importar
+# https://docs.python.org/release/3.12.3/library/math.html
+
+import random
+num = random.ranint(1,10)
+print(num)
+
+
+# Instalar biblioteca para exibir emojis no terminal
+# https://pypi.org/project/emoji/
