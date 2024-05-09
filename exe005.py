e402103a531eaf39ab8f92c45f65ebc00ebd7a5b
samsorce <samcodebeliver@gmail.com>
2024-05-09 10:02:08 -0300

    Create exe005.py

---
+++ exe005.py
@@ -0,0 +1,8 @@
+# Programa que leia um número inteiro e mostre na tela o seu secessor 
+# e seu antecessor.
+
+
+numero = int(input("Digite um número plz: "))
+
+print(f"O antecessor de {numero} é {numero - 1}")
+print(f"O sucessor   de {numero} é {numero + 1}")
