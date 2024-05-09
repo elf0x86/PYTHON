163c590baea0939e206654b01ac3078f6ac3a00c
samsorce <samcodebeliver@gmail.com>
2024-05-09 12:23:21 -0300

    Create exe010.py

---
+++ exe010.py
@@ -0,0 +1,8 @@
+# leia quanto dinheiro uma pessoa tem na carteira e mostre quantos 
+# Dolares ela pode compara 
+
+carteira = float(input("Quantos R$ voce tem: "))
+
+dolares = carteira * 0.19 
+
+print(f"com {carteira:.0f}R$ voce pode comprar {dolares:.0f}$")
