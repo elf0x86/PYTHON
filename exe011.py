faa2aa05b5c25f1648314fa824da2f05666f7312
samsorce <samcodebeliver@gmail.com>
2024-05-09 12:29:48 -0300

    Create exe011.py

---
+++ exe011.py
@@ -0,0 +1,12 @@
+# ler a largura altura de uma parede em metros, calcule a sua área e a 
+# quantidade de tinta necessária para pintála, sabendo que cada litro
+# de tinta, pinta uma área de 2m².
+
+largura = float(input("Largura: "))
+altura  = float(input("Altura:  "))
+
+area = largura * altura
+
+litros = area / 2
+
+print(f"Tinta necessaria para pintar uma area de {area}² é {litros}L de tinta.")
