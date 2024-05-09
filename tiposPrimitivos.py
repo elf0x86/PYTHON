acc5a6cafe62f3a6fd596c00746d2944bb232693
samsorce <samcodebeliver@gmail.com>
2024-05-08 23:19:52 -0300

    Create tiposPrimitivos.py

---
+++ tiposPrimitivos.py
@@ -0,0 +1,11 @@
+# tipos basicos
+
+inteiro = 5
+flutuante = 5.3
+booleano = True
+string = "Guanabara"
+
+print(inteiro,   type(inteiro))
+print(flutuante, type(flutuante))
+print(booleano,  type(booleano))
+print(string,    type(string))
