373296b7c13aacc7b1dbbae6b37a8cef15d91337
samsorce <samcodebeliver@gmail.com>
2024-05-09 10:37:11 -0300

    Create exe008.py

---
+++ exe008.py
@@ -0,0 +1,10 @@
+# Ler um valor em metros e o exiba convertido em centímetros e milímetros
+
+
+metros = int(input("Qual é o valor em metros: "))
+
+metros_para_centimetros = metros * 100
+metros_para_milimetros  = metros * 1000
+
+print(f"{metros}M = {metros_para_centimetros:.0f}cm")
+print(f"{metros}M = {metros_para_milimetros:.0f}mm")
