4a9a4e2f446d983a4586866c7dc2ce4038cabe82
samsorce <samcodebeliver@gmail.com>
2024-05-10 14:04:48 -0300

    Update exe021.py

--- exe021.py
+++ exe021.py
@@ -1,4 +1,49 @@
 # faça um programa que toque uma musica no terminal
 
 import os
-os.system('celluloid ./musica.wav')
+import pygame
+
+
+# os.system('celluloid ./musica.wav')
+
+"""
+pygame.init()
+pygame.mixer.music.load('./musica.wav')
+pygame.mixer.music.play()
+pygame.event.wait()
+"""
+
+from pygame import mixer
+
+# Starting the mixer
+mixer.init()
+
+# Loading the song
+mixer.music.load("musica.wav")
+
+# Setting the volume
+mixer.music.set_volume(0.7)
+
+# Start playing the song
+mixer.music.play()
+
+# infinite loop
+while True:
+
+    print("Press 'p' to pause, 'r' to resume")
+    print("Press 'e' to exit the program")
+    query = input("  ")
+
+    if query == 'p':
+
+        # Pausing the music
+        mixer.music.pause()
+    elif query == 'r':
+
+        # Resuming the music
+        mixer.music.unpause()
+    elif query == 'e':
+
+        # Stop the mixer
+        mixer.music.stop()
+        break
