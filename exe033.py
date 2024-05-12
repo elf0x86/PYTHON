669e3c2fa7fdf3a53ee317e49052365e755cf768
samsorce <samcodebeliver@gmail.com>
2024-05-12 11:59:33 -0300

    Create exe033.py

---
+++ exe033.py
@@ -0,0 +1,25 @@
+# Ler três números e mostre qual é o maior e qual é o menor
+
+a = int(input('Primeiro valor: '))
+b = int(input('Segundo valor : '))
+c = int(input('Terceiro valor: '))
+
+'''
+if a < b and a < c:
+    menor = a
+if b < c and b < a:
+    menor = b
+if c < a and c < b:
+    menor = c
+'''
+
+menor = a  # assumir que é o menor 
+nome_input = 'a'
+if b < a and b < c:
+    menor = b
+    nome_input = 'b'
+if c < a and c < b:
+    menor = c
+    nome_input = 'c'
+
+print(f'O menor valor é {menor} e o nome da variavel é: {nome_input}')
