aded698aa1ef8cfcce67c6207c932e5772b6171d
samsorce <samcodebeliver@gmail.com>
2024-05-12 10:39:58 -0300

    Create exe030.py

---
+++ exe030.py
@@ -0,0 +1,10 @@
+# Ler um número inteiro e mostre na tela se ele é PAR ou IMPAR
+
+
+numero = int(input("Me diga um número qualquer: "))
+
+if numero % 2 == 0:
+    print(f'O número {numero} é PAR')
+else:
+    print(f'O número {numero} é ÍMPAR')
+
