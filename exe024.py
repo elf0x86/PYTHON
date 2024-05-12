21657034e8c4e886b32b1a96f933641a5c60a7e4
samsorce <samcodebeliver@gmail.com>
2024-05-12 06:57:25 -0300

    Create exe024.py

---
+++ exe024.py
@@ -0,0 +1,6 @@
+# ler o nome de uma cidade e diga se ela começa ou não com o nome 'santo'
+
+cidade = input("Em que cidade você nasceu? ").lower()
+
+#print(cidade.find('santo'))
+print('santo' in cidade)
