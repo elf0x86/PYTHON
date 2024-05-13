10cdd678df023eaff1248aa33b64c455b64d43b7
samsorce <samcodebeliver@gmail.com>
2024-05-13 09:52:45 -0300

    Update loopComVariavelDeControle.py

--- loopComVariavelDeControle.py
+++ loopComVariavelDeControle.py
@@ -1,11 +1,26 @@
 import os
 
-
 '''
+# Conta de 0 a 9 (10 não é incluido) de 1 em 1
 for x in range(0,10):
+    print(f"Variavel de controle x: {x}. Ex: os.system('touch ARQUIVO_{x}.txt')")
+
+# Conta de 0 a 9 (10 não é incluido) de 2 em 2
+for x in range(0,10,2):
+    print(f"Variavel de controle x: {x}. Ex: os.system('touch ARQUIVO_{x}.txt')")
+
+
+# Conta de 10 a 0 de 1 em 1
+for x in range(10, 0, -1):
+    print(f"Variavel de controle x: {x}. Ex: os.system('touch ARQUIVO_{x}.txt')")
+print('FIM')
+
+# Conta de 10 a 0 de 1 em 2
+for x in range(10, 0, -2):
     print(f"Variavel de controle x: {x}. Ex: os.system('touch ARQUIVO_{x}.txt')")
 print('FIM')
 '''
+
 
 escolha = int(input('Digite [ 1 ] para criar [ 0 ] para apagar ARQUIVO_X.txt: '))
 for x in range(0,10):
@@ -15,5 +30,3 @@ for x in range(0,10):
         os.system(f'rm ARQUIVO_{x}.txt')
     else:
         print('Algo deu ERRADO!')
-
-
