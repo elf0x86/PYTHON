ea1484fc72d96cdc21eee55e2aea33323ec54dbd
samsorce <samcodebeliver@gmail.com>
2024-05-13 21:24:33 -0300

    Create exe007.py

---
+++ exe007.py
@@ -0,0 +1,8 @@
+# Lear duas notas de um aluno, clacule e mostre a sua média
+
+nota_1 = float(input("Entre com a primeira nota: "))
+nota_2 = float(input("Entre com a segunada nota: "))
+
+media = (nota_1+nota_2) / 2
+
+print(f"A média das notas: {media}")
