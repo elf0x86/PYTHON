f9caf2d701387bfc13aab38c9a05c0f55ed67cc8
samsorce <samcodebeliver@gmail.com>
2024-05-18 16:50:39 -0300

    Update exe065.py

--- exe065.py
+++ exe065.py
@@ -24,3 +24,23 @@ print(f'Total: {tot_input}')
 print(f'Soma : {soma}')
 print(f'Média: {media}')
 print(f'Maior: {maior}, Menor: {menor}')
+
+# Solução do video ------------------------------------------------------------------------------
+resp = 'S'
+soma = quant = media = maior = menor= 0
+while resp in 'Ss':
+    num = int(input('Digite um número: '))
+    soma = soma + num
+    quant = quant + 1
+    if quant == 1:
+        maior = menor = num
+    else:
+        if num > maior:
+            maior = num
+        if num < menor:
+            menor = num
+
+    resp = input('Quer continuar? [S/N]: ').upper().strip()[0]
+media = soma / quant
+print(f'Você digitou {quant} numeros e a media foi {media}')
+print(f'O maior valor foi {maior} e o menor foi {menor}')
