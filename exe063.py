4078ee4e212945eae73245eee054c6781a0cc1e1
samsorce <samcodebeliver@gmail.com>
2024-05-18 11:01:46 -0300

    Update exe063.py

--- exe063.py
+++ exe063.py
@@ -19,4 +19,38 @@ while n != 0:
 
    n -= 1
 
+# Solução do video -----------------------------------------------------------------------
+print('-' * 30)
+print('Sequência de Fibonacci')
+print('-' * 30)
+n = int(input('Quantos termos você quer mostrar? '))
+t1 = 0
+t2 = 1
+print('~'*30)
+print(f'{t1} -> {t2}', end='')
+cont = 3
+while cont <= n:
+    t3 = t1 + t2
+    print(f' -> {t3}', end='')
+    t1 = t2
+    t2 = t3
+    cont += 1
+print(' -> FIM')
+
+# 0 - 1 - 1
+# t1 t2  t3
+
+# 0 - 1 - 1 - 2
+#    t1  t2  t3
+
+# 0 - 1 - 1 - 2 - 3
+#        t1  t2  t3
+
+# 0 - 1 - 1 - 2 - 3 - 5
+#            t1  t2  t3
+
+# 0 - 1 - 1 - 2 - 3 - 5 - 8
+#                t1  t2  t3
+
+
 
