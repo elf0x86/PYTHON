8df4681fc989c0502d6749bb738f23d362b24bbe
samsorce <samcodebeliver@gmail.com>
2024-05-18 16:50:13 -0300

    Update exe064.py

--- exe064.py
+++ exe064.py
@@ -23,4 +23,20 @@ while num != 999:
     cont += 1
 
 print(f'Você digitou {cont - 1} números. E a soma entre eles foi {soma - 999}')
+print(soma) # ainda tem 999 somado junto as opçoes
+soma = soma - 999 # sulução para o proplema tirar 999 da soma apos o loop
+print(soma) # valor desejado 
+
+# Solução 2 do video  ----------------------------------------------------------------------
+num  = 0
+cont = 0
+soma = 0
+num = int(input('Digite um numero [999 para parar]: '))
+while num != 999:
+    soma += num
+    cont += 1
+    num = int(input('Digite um numero [999 para parar]: '))
+
+print(f'Você digitou {cont} números. E a soma entre eles foi {soma}')
+
 
