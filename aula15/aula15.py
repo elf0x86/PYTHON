87e2aeee9eb8c9c651d9ae34c916e8b71cb9996a
samsorce <samcodebeliver@gmail.com>
2024-05-18 18:51:57 -0300

    Create aula15.py

---
+++ aula15/aula15.py
@@ -0,0 +1,23 @@
+# REPETIÇÃO USANDO UMA "FLAG" COMO PONTO DE PARADA
+n = 0
+while n != 999:
+    n = int(input('Digite um número: '))
+    
+
+# REPETIÇÃO INFINITA USANDO CONDIÇÃO DE PARADA 
+n = s = 0
+while True:
+    n = int(input('Digite um número: '))
+    if n == 999:
+        break
+    s += n
+    
+
+'''
+Atualização nova do python 2017
+pep (python enhancement proposal)
+pep ...
+pep 498 - fstring
+
+link: https://peps.python.org/pep-0498/
+'''
