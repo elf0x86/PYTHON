7f30769a4d672f398a3de07274f12f7a48c26cfc
samsorce <samcodebeliver@gmail.com>
2024-05-17 21:38:09 -0300

    Create exe065.py

---
+++ exe065.py
@@ -0,0 +1,26 @@
+# todos os valores e qual foi o maior e o menor valores lidos. O programa deve perguntar ao
+# usuário se ele quer ou não continuar a digitar valores.
+
+
+soma = valor = maior = menor = media = tot_input = stop_condition = 0
+
+while stop_condition != 999:
+    valor = int(input("Digite um valor: "))
+    soma += valor
+    if stop_condition == 0:
+        maior = valor
+        menor = valor
+
+    if maior < valor:
+        maior = valor
+    if menor > valor:
+        menor = valor
+
+    tot_input += 1
+    stop_condition = int(input("[ 1 ]Continuar\n[ 999 ] Para sair."))
+
+media = soma / tot_input
+print(f'Total: {tot_input}')
+print(f'Soma : {soma}')
+print(f'Média: {media}')
+print(f'Maior: {maior}, Menor: {menor}')
