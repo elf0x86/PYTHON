7940896650dc8002a898b198cf7591c1197508a8
samsorce <samcodebeliver@gmail.com>
2024-05-19 09:43:59 -0300

    Update exe067.py

--- exe067.py
+++ exe067.py
@@ -11,3 +11,16 @@ while True:
             print(f'{x: >2} x {n} = {x*n: >2}')
 
 
+# Solução do video ----------------------------------------------------------------------------------------
+
+while True:
+    n = int(input('Quer a tabuada de qual valor? '))
+    if n < 0:
+        break
+    print('-' * 30)
+    for c in range(1,11):
+        print(f'{n} x {c} = {n*c}')
+    print('-' * 30)
+
+print('PROGRAMA TABUADA ENCERRADO. Volte sempre!')
+
