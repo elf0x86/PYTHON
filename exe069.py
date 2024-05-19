7be31e44638e3c308d4a14de0007698955e51062
samsorce <samcodebeliver@gmail.com>
2024-05-19 12:32:00 -0300

    Update exe069.py

--- exe069.py
+++ exe069.py
@@ -37,3 +37,32 @@ while True:
 print(f'Total de maiores de 18 anos cadastrados {maior_idade}.')
 print(f'Total de homens cadastrados {total_homens}.')
 print(f'{mulheres}  mulheres com menos de 20 anos.')
+
+# Solução do video ----------------------------------------------------------------
+
+tot18 = totH = totM20 = 0
+
+while True:
+    idade = int(input('Idade: '))
+    sexo = ' '
+    while sexo not in 'MF':
+        sexo = str(input('Sexo [M/F]')).strip().upper()[0]
+
+    if idade >= 18:
+        tot18 += 1
+    if sexo == 'M':
+        totH += 1
+    if sexo == 'F' and idade < 20:
+        totM20 += 1
+
+
+    resp = ' '
+    while resp not in 'SN':
+        resp = str(input('Quer continuar? [S/N]: ')).strip().upper()[0]
+    if resp == 'N':
+        break
+
+print(f'Total de pessoas com mais de 18 anos: {tot18}')
+print(f'Ao todo temos {totH} homens cadastrados')
+print(f'E temos {totM20} mulheres com menos de 20 anos')
+
