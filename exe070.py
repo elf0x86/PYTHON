8d46d28b6fddc50a05684038810d95d0d73785b5
samsorce <samcodebeliver@gmail.com>
2024-05-19 13:31:27 -0300

    Update exe070.py

--- exe070.py
+++ exe070.py
@@ -38,3 +38,34 @@ while True:
 print(f'Total gasto na compra R$: {total_gasto}')
 print(f'{custa_mais_mill} produto(s) que custam mais de R$1000')
 print(f'Nomo do produto mais barato é {nome_produto_barato}')
+
+# Solução do video ------------------------------------------------------------------
+total = totmil = menor = cont = 0
+barato = ''
+
+while True:
+    produto = str(input('Nome do Produto: '))
+    preco   = float(input('Preço R$: '))
+    cont += 1 
+    total += preco 
+    if preco > 1000:
+        totmil += 1
+
+    if cont == 1:
+        menor = preco
+        barato = produto
+    else:
+        if preco < menor:
+            menor = preco
+            barato = produto
+    resp = ' '
+    while resp not in 'SN':
+        resp = str(input('Quer continuar? [S/N]: ')).strip().upper()[0]
+    if resp == 'N':
+        break
+print(f'{"FIM DO PROGRAMA":-^40}')
+print(f'O total da compra foi: {total:.2f}')
+print(f'Temos {totmil} produtos custando mais de R$1000')
+print(f'O produto mais barato foi {barato} que custa R${menor:.2f}')
+
+
