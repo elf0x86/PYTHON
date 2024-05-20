dd579230a332870414e15fade81e213418b2d210
samsorce <samcodebeliver@gmail.com>
2024-05-20 10:08:10 -0300

    Create exe072.py

---
+++ exe072.py
@@ -0,0 +1,22 @@
+# Crie um porgrama que tenha uma tupla totalmente preenchida com uma contagem
+# por extenso, de zero até vinte. Seu programa deverá ler um número pelo
+# teclado (entre 0 e 20) e mostrá-lo por extenso.
+
+contagem_extenso = ('zero','um', 'dois', 'tres', 'quatro', 'cinco', 'seis', 'sete',
+                    'oito', 'nove', 'dez', 'onze', 'doze', 'treze', 'quatorze'
+                    'quinze', 'deseseis', 'desesete', 'dezoito', 'dezenove',
+                    'vinte')
+
+while True:
+    numero = int(input('Digite um número de 0 a 20: '))
+    if numero <= 20:
+        print(f'Você digitou o número \033[1;32m{contagem_extenso[numero]}\033[m')
+    else:
+        print('\033[1;33mNUMERO INVALIDO TENTE NOVAMENTE!\033[m')
+
+    flag = input('Quer continuar [S/N]: ').strip().upper()[0]
+    if flag == 'N':
+        break
+    else:
+        continue
+
