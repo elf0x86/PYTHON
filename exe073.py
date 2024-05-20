04e777f04041c2bcb7aceb6ee447537c1dfd28ee
samsorce <samcodebeliver@gmail.com>
2024-05-20 10:26:38 -0300

    Update exe073.py

--- exe073.py
+++ exe073.py
@@ -33,4 +33,20 @@ if 'Chapecoense' in times:
 else:
     print(f'Chapeconese não esta na lista')
 
+# Solução do video ------------------------------------------------------------
+times = ("Palmeiras", "Bahia","Flamengo","Botafogo","São Cruzeiro","Atlético","Bragantino",
+         "Figueirence","Internacional","Fortaleza","Grêmio","Vasco","Criciúma","Juventude",
+         "Barcelona","Corinthians","Fluminense","Vitória","Atlético","Cuiabá")
+
+print('-=' * 15)
+print(f'Lista de times do Brasileirão: {times}')
+print('-=' * 15)
+print(f'Os 5 primeiros times são {times[0:5]}')
+print('-=' * 15)
+print(f'Os 4 últimos são {times[-4:]}')
+print('-=' * 15)
+print(f'Times em ordem alfabética: {sorted(times)}')
+print('-=' * 15)
+print(f'O Capecoense esta na {times.index("Chapecoense")+1} posição')
+
 
