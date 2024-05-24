70fe99519506e8daae166047c5652969d9922d05
samsorce <samcodebeliver@gmail.com>
2024-05-24 20:19:51 -0300

    Update exe078.py

--- exe078.py
+++ exe078.py
@@ -31,4 +31,30 @@ for x in range(0, len(valores)):
 
 print(f'O maior valor digitado foi {maior} nas posições {maior_position}')
 print(f'O menor valor digitado foi {menor} nas posições {menor_position}')
-~                                                                        
+
+# SOLUÇÃO DO VIDEO ---------------------------------------------------------------------
+listanum = []
+mai = 0
+men = 0
+for c in range(0, 5):
+    listanum.append(int(input(f'Digite um valor para a posição {c}: ')))
+    if c == 0:
+        mai = men = listanum[c]
+    else:
+        if listanum[c] > mai:
+            mai = listanum[c]
+        if listanum[c] < men:
+            men = listanum[c]
+print('=-' * 30)
+print(f'Você digitou os valores {listanum}')
+print(f'O maior valor digitado foi {mai} nas posições ', end='')
+for i,v in enumerate(listanum):
+    if v == mai:
+        print(f'{i}... ', end='')
+print()
+print(f'O menor valor digitado foi {men} nas posiçẽos ', end='')
+for i, v in enumerate(listanum):
+    if v == men:
+        print(f'{i}... ', end='')
+print()
+
