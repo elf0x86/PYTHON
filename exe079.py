c0ea3a679457929dff00c30656048de0e840e368
samsorce <samcodebeliver@gmail.com>
2024-05-24 20:26:12 -0300

    Update exe079.py

--- exe079.py
+++ exe079.py
@@ -24,3 +24,19 @@ while resp not in 'N':
 print('=-' * 30)
 valores.sort()
 print(f'Você digitou os valores {valores}')
+
+# SOLUÇÃO DO VIDEO ----------------------------------------------------------------------------------
+numeros = list()
+while True:
+    n = int(input('Digite um valor: '))
+    if n not in numeros:
+        numeros.append(n)
+        print('Valor addicionado com sucesso...')
+    else:
+        print('Valor duplicado! Não vou adicionar...')
+    r = str(input('Quer continuar? [S/N]: '))
+    if r in 'Nn':
+        break
+print('-=' * 30)
+numeros.sort()
+print(f'Você digitou os valores {numeros}')
