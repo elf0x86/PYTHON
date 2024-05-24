0846b2cddb09ce1e461435153246401aa7d9e164
samsorce <samcodebeliver@gmail.com>
2024-05-24 20:37:35 -0300

    Update exe082.py

--- exe082.py
+++ exe082.py
@@ -27,3 +27,22 @@ print('=-' * 30)
 print(f'A lista completa é: {lista}')
 print(f'A lista de pares é: {lista_par}')
 print(f'A lista de impares é: {lista_impar}')
+
+# SOLUÇÃO DO VIDEO -----------------------------------------------------------------------
+num = list()
+pares = list()
+impares = list()
+while True:
+    num.append(int(input('Digite um número: ')))
+    resp = str(input('Quer continuar? [S/N]: '))
+    if resp in 'Nn':
+        break
+for i, v in enumerate(num):
+    if v % 2 == 0:
+        pares.append(v)
+    elif v % 2 == 1:
+        impares.append(v)
+print('-=' * 30)
+print(f'A lista completa é {num}')
+print(f'A lista de pares é {pares}')
+print(f'A lista de impares é {impares}')
