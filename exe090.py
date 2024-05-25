135351f2904a1f41fb009c61eef433605c4405ce
samsorce <samcodebeliver@gmail.com>
2024-05-25 12:55:58 -0300

    Update exe090.py

--- exe090.py
+++ exe090.py
@@ -17,3 +17,17 @@ else:
 print(f'Nome     é igual a {aluno["nome"]}')
 print(f'Média    é igual a {aluno["media"]}')
 print(f'Situação é igual a {aluno["situacao"]}')
+
+# SOLUÇÃO DO VIDEO ----------------------------------------------------------------------
+aluno = dict()
+aluno['nome'] = str(input('Nome: '))
+aluno['media'] = float(input(f'Media de {aluno["nome"]}: '))
+if aluno['media'] >= 7:
+    aluno['situacao'] = 'Aprovado'
+elif 5 <= aluno['media'] < 7:
+    aluno['situcao'] = 'Recuperação'
+else:
+    aluno['situacao'] = 'Reprovado'
+print('-=' * 30)
+for k, v in aluno.items():
+    print(f' - {k} é igual a {v}')
