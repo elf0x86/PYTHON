51819df409dcad63fc354c639100af615360a847
samsorce <samcodebeliver@gmail.com>
2024-05-25 09:08:32 -0300

    Update exe088.py

--- exe088.py
+++ exe088.py
@@ -28,4 +28,33 @@ for x in range(0, len(jogos)):
 
 print(f'{" < BOA SORTE! > ":=^40}')
 
-# <shift> + # + k open the manuals of python. Similar to help('if') 
+# <shift> + # + k open the documentation of python. Similar to help('if') 
+# SOLUÇÃO DO VIDEO -------------------------------------------------------------------------------
+from random import randint
+from time   import sleep
+lista = list()
+jogos = list()
+print('-' * 30)
+print('    JOGA NA MEGASENA    ')
+print('-' * 30)
+quant = int(input('Quanto jogos você quer que eu sorteie? '))
+tot = 1
+while tot <= quant:
+    cont = 0
+    while True:
+        num = randint(1, 60)
+        if num not in lista:
+            lista.append(num)
+            cont += 1
+        if cont >= 6:
+            break
+    lista.sort()
+    jogos.append(lista[:])
+    lista.clear()
+    tot += 1
+print('-=' * 3, f'SORTEANDO {quant} JOGOS', '=-' * 3)
+for i, l, in enumerate(jogos):
+    print(f'Jogo {i+1}: {l}')
+    sleep(0.5)
+print('-=' * 5, '< BOA SORTE >', '=-' * 5)
+
