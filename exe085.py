1b703f8c5e153793bf35536b8ce1ab78ba39103c
samsorce <samcodebeliver@gmail.com>
2024-05-25 08:13:00 -0300

    Update exe085.py

--- exe085.py
+++ exe085.py
@@ -23,3 +23,18 @@ print(f'Lista de valores pares {pares}')
 
 impares.sort(reverse=True)
 print(f'Lista de valores impares {impares}')
+
+# SOLUÇÃO DO VIDEO -------------------------------------------------------------------
+num = [[], []]
+valor = 0
+for c in range(1, 8):
+    valor = int(input(f'Digite o {c}ª valor: '))
+    if valor % 2 == 0:
+        num[0].append(valor)
+    else
+        num[1].append(valor)
+print('-=' * 30)
+num[0].sort()
+num[1].sort()
+print(f'Os valores pares   digitados foram {num[0]}')
+print(f'Os valores impares digitados foram {num[0]}')
