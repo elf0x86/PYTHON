13b2ff31f0ce2bb4d14841088ce0457b43923500
elf0x86 <samcodebeliver@gmail.com>
2025-01-03 21:57:30 -0300

    Create variables_expressions_statements.py

---
+++ pyForEverybody/variables_expressions_statements.py
@@ -0,0 +1,99 @@
+'''
+Vocabulary / Words - variables and Reserved words -- RESERVED KEYWORDS can't be used as variables
+  False 	  def 	    if 	      raise
+  None 	    del 	    import 	  return
+  True 	    elif 	    in 	      try
+  and 	    else 	    is 	      while
+  as 	      except 	  lambda 	  with
+  assert 	  finally 	nonlocal 	yield
+  break 	  for 	    not 	
+  class 	  from 	    or 	
+  continue 	global 	  pass
+Sentence structure - valis syntax patterns
+  x = 2      -> Assignment statement
+  x = x + 2 -> Assignment with expression
+  print(x)  -> Print statement
+  VARIABLE  OPERATOR  CONSTANT  RESERVED_WORD
+  x         = +       2         print
+Story structure    - constructing a program for a purpose
+  
+'''
+
+# Sequential  code
+# Repeated    code
+# Conditional code
+
+# Constants data
+print(124)
+
+# Variables
+x = 12.34
+y = 15
+
+# Reasingment 
+x = 100
+
+# GOOD: span eggs span32 _speed
+# BAD : 43span #sign var.12
+# Different: span != Span != SPAN
+
+
+asdr9q24u = 35.0
+nn1234bks = 12.5
+al35hr923 = asdr9q24u * nn1234bks
+print(al35hr923)
+
+a = 35.0
+b = 12.5
+c = a + b
+print(c)
+
+hours = 35.0
+rate = 12.5
+pay = hours * rate
+print(pay)
+
+# Evaluates the expression to the right side of = then assign to x
+x = 2
+x = 3.8 * x * ( 1 - x )
+print(x)
+
+# Expressions - Operators [+ - * / ** %]
+a = 2
+b = 5
+print(a + b)
+print(b - a)
+print(b / a)
+print(b ** a)
+print(b % a)
+
+# Operators Precedence rules 
+# 1. ()
+# 2. **
+# 3. * /
+# 4. + -
+# 5. left-to-right
+print(1 + 2 ** 3 / 4 * 5) # 11
+
+# What does 'Type' Mean?
+w = 15.6
+x = 123
+y = 'abc'
+z = True
+print(type(w))
+print(type(x))
+print(type(y))
+print(type(z))
+
+# Type conversions
+w = int(w)
+x = float(x)
+y = bool(y)
+z = int(z)
+
+# Integer Division to float
+print(10 / 2) # 5.0
+print(9  / 2) # 4.5
+print(99 / 100) # 0.99
+print(10.0 / 2.0) # 5.0
+
