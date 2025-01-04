8eb6fecb267fd74208ab1441ee456a20faf9546d
elf0x86 <samcodebeliver@gmail.com>
2025-01-03 22:56:27 -0300

    Create user_input.py

---
+++ pyForEverybody/user_input.py
@@ -0,0 +1,12 @@
+name = input('Who are you?: ')
+print('Welcome', name)
+
+inp = input("Europe floor?: ")
+usr = int(inp) + 1
+print('US floor', usf)
+
+# Python interpreter does not interpret comment
+
+
+
+
